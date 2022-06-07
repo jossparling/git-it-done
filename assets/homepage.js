cb5639a9a5cb3513535128cf9832fac87a6b55e6
jossparling <jossparling@gmail.com>
2022-06-07 07:51:16 -0500

    added form element

--- assets/homepage.js
+++ assets/homepage.js
@@ -10,4 +10,22 @@ var getUserRepos = function(user) {
     });
   };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+  };
+
+// get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
